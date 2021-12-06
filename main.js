54f4f28a923bcfce04f79b6b216c9954a6d7e7e7
Anirudh Iyer <anirudh.iyer@outlook.com>
2021-12-06 12:33:02 -0500

    mobile responsive

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+ const mobilehover = document.getElementsByClassName('tst')
+
+
+ const onMobileClick = (target) => {
+    mobilehover[0].id = 'newclass'
+    // window.alert('WORKED') mobile click should be working
+ }
+ 
  
