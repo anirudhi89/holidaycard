787ba569125eb270886728bcb333e53d525c99cb
Anirudh Iyer <anirudh.iyer@outlook.com>
2021-12-08 13:58:51 -0500

    error handling, mobile ubpadtes

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
- const mobilehover = document.getElementsByClassName('tst')
+const mobilehover = document.getElementsByClassName('tst')
+var x = window.matchMedia("(max-width: 700px)")
+function driver() {
+   mobileResponsive(x)
+}
 
 
- const onMobileClick = (target) => {
-    mobilehover[0].id = 'newclass'
-    // window.alert('WORKED') mobile click should be working
+
+//  const onMobileClick = (target) => {
+//     mobilehover[0].id += 'newclass'
+//    //  window.alert('WORKED') //mobile click should be working
+    
+//  }
+ const mobileResponsive = (x) => {
+   if (x.matches) { //media query passed
+      mobilehover[0].setAttribute('id','newclass')
+      console.log(mobilehover[0])
+      window.alert('WORKED') 
+      window.alert('WORKED') 
+   }
+   else {
+      window.alert('error')
+      console.log(mobilehover[0])
+   }
+  
+   
  }
- 
- 
+
+//  x.addListener(mobileResponsive)
